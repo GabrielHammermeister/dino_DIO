fb778449014778d802994d5a9fee2101a2d4c398
Gabriel Hammermeister <48499810+GabrielHammermeister@users.noreply.github.com>
2021-01-23 10:31:10 -0300

    cactus and jump

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
-const dino = document.querySelector(".dino")
+const dino = document.querySelector('.dino')
+const background = document.querySelector(".background")
 
-dino
+let isJumping = false
 
 function handleKeyUp(event) {
 
     if(event.keyCode === 32){
-        console.log("espaco")
+        if(!isJumping){
+            jump()
+        }
     }
 
 }
+function createCactus() {
 
+    const cactus = document.createElement('div')
+
+
+    cactus.classList.add('cactus')
+    cactus.style.left = 1000 + 'px'
+    background.appendChild(cactus)
+}
+
+function jump() {
+    isJumping = true
+    let position = 0
+
+    let upInterval = setInterval(()=> {
+        
+        if(position >= 150) {
+            clearInterval(upInterval)
+
+            let downInterval = setInterval(() => {
+
+                if(position <= 0) {
+                    isJumping = false
+                    clearInterval(downInterval)
+                } else {
+                    position -= 20
+                    dino.style.bottom = position + 'px'
+                }
+
+            }, 15)
+
+        } else {
+            position += 20
+            dino.style.bottom = position + 'px'
+        }
+
+
+    }, 15)
+
+}
+
+createCactus()
 document.addEventListener("keyup", handleKeyUp);
